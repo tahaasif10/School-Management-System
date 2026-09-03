757f42fea42da3fdc041e4a51b6997ed25b78a59
HuzaifaAsif <drhuzaifaasif@gmail.com>
2026-09-03 11:12:14 -0700

    Update lucide-react to version 1.40.0 and implement Staff Management component

---
+++ frontend/src/app/teachers/page.tsx
@@ -0,0 +1,9 @@
+import StaffManagement from "../../components/StaffManagement/StaffManagement";
+
+export default function TeachersPage() {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <StaffManagement />
+    </div>
+  );
+}
